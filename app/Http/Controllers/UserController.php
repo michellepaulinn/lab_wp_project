c9dee7f34dc6258f474e5d477deb66887af10fca
Michelle Pauline <michellepaulinn@gmail.com>
2022-06-24 21:12:52 +0700

    updated login authentication

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
@@ -17,7 +18,7 @@ class UserController extends Controller
         $validation = Validator::make($request->all(),
         [
             'name' => 'required',
-            'email' => 'required|email|unique:users,email', //unique: //tanya cecenya unique apaan 
+            'email' => 'required|email:dns|unique:users,email', //unique: //tanya cecenya unique apaan 
             'password' => 'required|min:8|confirmed',
             // 'password2' => 'required_with:password|same:password' //pass samainnya bener kek gini 
         ]
@@ -33,7 +34,9 @@ class UserController extends Controller
         $newMember->name = $request->name;
         $newMember->email = $request->email;
         //hash passwordnya
-        $newMember->password = Hash::make($request->password);
+
+        // $newMember->password = Hash::make($request->password);
+        $newMember->password = bcrypt($request->password);
         // $newMember->password = $request->password;
         $newMember->save();
 
@@ -44,32 +47,51 @@ class UserController extends Controller
         return view('login');
     }
 
-    public function loginProcess(Request $request){
-        $validation = Validator::make($request->all(),
-        [
-            'email' => 'required|email', 
-            'password' => 'required|min:8'
-        ]
-        );
-        if($validation->fails()){
-            return redirect()->back()->withErrors($validation);
-        }
-        //validasiin ke database
-        $membData = User::where('email', $request->email)->first(); //data di db
-        //kalau gaada
-        if(! $membData) { // ga tau null di sini tulis apa 
-            return redirect()->back()->withErrors("You don't have an account yet");
-        }
-        else{
-            // $pass = Hash::make($request->password);
-            $pass = $request->password;
-            if (! Hash::check($pass, $membData->password)){
-                return redirect()->back()->withErrors('Password Mismatch');
-            }
-            else{ 
-                return redirect('/');
-            }
-        }
+    // public function loginProcess(Request $request){
+    //     $validation = Validator::make($request->all(),
+    //     [
+    //         'email' => 'required|email', 
+    //         'password' => 'required|min:8'
+    //     ]
+    //     );
+    //     if($validation->fails()){
+    //         return redirect()->back()->withErrors($validation);
+    //     }
+    //     //validasiin ke database
+    //     $membData = User::where('email', $request->email)->first(); //data di db
+    //     //kalau gaada
+    //     if(! $membData) { // ga tau null di sini tulis apa 
+    //         return redirect()->back()->withErrors("You don't have an account yet");
+    //     }
+    //     else{
+    //         // $pass = Hash::make($request->password);
+    //         $pass = $request->password;
+    //         if (! Hash::check($pass, $membData->password)){
+    //             return redirect()->back()->withErrors('Password Mismatch');
+    //         }
+    //         else{ 
+    //             return redirect('/');
+    //         }
+    //     }
         
+    // }
+
+    public function loginProcess(Request $request){
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+ 
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+ 
+            return redirect()->intended('/');
+        }
+ 
+        return back()->withErrors([
+            'errors' => 'The provided credentials do not match our records.',
+        ]);
     }
+    
+
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -1,19 +1,20 @@
 <?php
 
-namespace App\Models;
+namespace App\Models ;
 
+use Illuminate\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class User extends Model
+class User extends Model implements \Illuminate\Contracts\Auth\Authenticatable
 {
     use HasFactory;
+    use Authenticatable;
 
     protected $guarded=['id'];
 
     public function cart(){
         return $this->hasOne(Cart::class);
     }
-
     
 }
