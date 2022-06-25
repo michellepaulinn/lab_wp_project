9e280f3a094798008a17ebc2cd54ccfdef07a6b0
Michelle Pauline <michellepaulinn@gmail.com>
2022-06-25 17:54:09 +0700

    update navbar view

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -63,7 +63,7 @@
               <a class="nav-link" href="/">Dashboard <span class="sr-only"></span></a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Cart</a>
+              <a class="nav-link" href="/cart">Cart</a>
             </li>
             @can('admin')
                 <li class="nav-item dropdown">
@@ -78,14 +78,22 @@
         </div>
         <div>
             @guest
-            <h6>GUEST</h6>    
+            <div class="d-flex ">
+                <h6 class="align-middle p-2">GUEST</h6>
+                <a href="/login" class="p-1">LOGIN</a>
+            </div>   
             @endguest
             @auth
             <div class="d-flex">
                 <h6>{{auth()->user()->name}}</h6>
                 <form action="/logout" method="post">
                     @csrf
-                    <button type="submit"><i class="bibi-box-arrow-right"></i></button>
+                    <button class="btn align-middle mb-3" type="submit">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"/>
+                            <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
+                        </svg>
+                    </button>
                 </form>
             </div>
             @endauth
